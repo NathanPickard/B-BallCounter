3ab5b00215b9fab8e8fbc0da57292a3a27fa7dd6
Nathan Pickard <nathan.pickard87@gmail.com>
2016-06-19 13:38:12 -0700

    Added Load Game menu item to MainActivity

--- app/src/main/java/apps/nathanpickard/BBallCounter/MainActivity.java
+++ app/src/main/java/apps/nathanpickard/BBallCounter/MainActivity.java
@@ -3,6 +3,7 @@ package apps.nathanpickard.BBallCounter;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -58,14 +59,14 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-    /*
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_main_activity, menu);
         return true;
     }
-    */
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
